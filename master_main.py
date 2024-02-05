53351ee2381ee27528df0438c8f123cb0b003e3e
Federico Torres <federico.torres@mulesoft.com>
2024-02-05 12:29:45 -0300

    feat: src package structure + leaked secret

--- master_main.py
+++ master_main.py
@@ -1,2 +1,14 @@
 # this is the beggining of the project
 # main file
+
+
+# Master Main file content
+# Master Main file content
+# Master Main file content
+# Master Main file content
+# Master Main file content
+# Master Main file content
+
+
+if __name__ == '__main__':
+    print('Ok')
